44659e960f74069e7e9fa67f874df394437675d0
Brian Lalor <blalor@bravo5.org>
2016-05-30 06:13:19 -0400

    Configure logging for tests

--- submodule/submodule_suite_test.go
+++ submodule/submodule_suite_test.go
@@ -1,6 +1,8 @@
 package submodule_test
 
 import (
+    log "github.com/sirupsen/logrus"
+
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
 
@@ -8,6 +10,10 @@ import (
 )
 
 func TestSubmodule(t *testing.T) {
+    // initialize logging and capture output with Ginkgo
+    log.SetLevel(log.DebugLevel)
+    log.SetOutput(GinkgoWriter)
+
     RegisterFailHandler(Fail)
     RunSpecs(t, "Submodule Suite")
 }
